7e9f6dd7cc603867bf02a66217d40a29bbbacece
madehim <madehim333@yandex.ru>
2019-07-24 18:04:08 +0400

    Add project files.

---
+++ OtsuBin/WinForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace testtest
+{
+    partial class BinForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayscaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.binaryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(262, 30);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(524, 30);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(781, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grayscaleToolStripMenuItem,
+            this.binaryToolStripMenuItem});
+            this.saveToolStripMenuItem.Enabled = false;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // grayscaleToolStripMenuItem
+            // 
+            this.grayscaleToolStripMenuItem.Enabled = false;
+            this.grayscaleToolStripMenuItem.Name = "grayscaleToolStripMenuItem";
+            this.grayscaleToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.grayscaleToolStripMenuItem.Text = "Grayscale";
+            this.grayscaleToolStripMenuItem.Click += new System.EventHandler(this.grayscaleToolStripMenuItem_Click);
+            // 
+            // binaryToolStripMenuItem
+            // 
+            this.binaryToolStripMenuItem.Enabled = false;
+            this.binaryToolStripMenuItem.Name = "binaryToolStripMenuItem";
+            this.binaryToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.binaryToolStripMenuItem.Text = "Binary";
+            this.binaryToolStripMenuItem.Click += new System.EventHandler(this.binaryToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 289);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(781, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(766, 17);
+            this.toolStripStatusLabel1.Spring = true;
+            // 
+            // BinForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 311);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(797, 349);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(797, 349);
+            this.Name = "BinForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Otsu Binarization";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayscaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem binaryToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ OtsuBin/WinForm.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace testtest
+{
+    public partial class BinForm : Form
+    {
+        public BinForm()
+        {
+            InitializeComponent();
+        }
+
+        string imagefilterof = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF";
+        string imagefiltersf = "*.BMP|*.BMP|*.JPG|*.JPG";
+        Bitmap GrayScaleImg;
+        Bitmap DefaultImg;
+        Bitmap BinImg;
+        byte BinThreshold = 0;
+
+        public static byte[,] BitmapToGrayScaleByte(Bitmap Img) // Метод для перевода изображения в байтовый массив
+        {
+            BitmapData bitmap_data = new BitmapData();
+            bitmap_data = Img.LockBits(new System.Drawing.Rectangle(0, 0, Img.Width, Img.Height),
+                                        ImageLockMode.ReadOnly, Img.PixelFormat);
+            int pixelsize = System.Drawing.Image.GetPixelFormatSize(bitmap_data.PixelFormat) / 8;
+
+            IntPtr pointer = bitmap_data.Scan0;
+            int nbytes = bitmap_data.Height * bitmap_data.Stride;
+            byte[] imagebytes = new byte[nbytes];
+            System.Runtime.InteropServices.Marshal.Copy(pointer, imagebytes, 0, nbytes);
+
+            double red;
+            double green;
+            double blue;
+            byte gray;
+
+            int helpint = 0;
+            int addint = 0;
+            if ((double)bitmap_data.Stride / (double)pixelsize != (double)bitmap_data.Width) // классный грязный хак!(изображение не "наклоняется")
+            {
+                if ((double)bitmap_data.Stride / (double)pixelsize - (double)bitmap_data.Width == 1)
+                {
+                    helpint = 1;
+                    addint = 1;
+                }
+                else
+                {
+                    if ((double)bitmap_data.Stride / (double)pixelsize - (double)(bitmap_data.Width) > 0.6)
+                        addint = 2;
+                    if ((double)bitmap_data.Stride / (double)pixelsize - (double)(bitmap_data.Width) < 0.6)
+                        addint = 3;
+                }
+            }
+            var _grayscale_array = new byte[bitmap_data.Height, bitmap_data.Width + addint];
+
+            if (pixelsize >= 3)
+            {
+                for (int I = 0; I < bitmap_data.Height; I++)
+                {
+                    for (int J = 0; J < bitmap_data.Width + helpint; J++)
+                    {
+                        int position = (I * bitmap_data.Stride) + (J * pixelsize);
+                        blue = imagebytes[position];
+                        green = imagebytes[position + 1];
+                        red = imagebytes[position + 2];
+                        gray = (byte)Math.Floor(0.299 * red + 0.587 * green + 0.114 * blue);
+                        _grayscale_array[I, J] = gray;
+                    }
+                }
+            }
+
+            Img.UnlockBits(bitmap_data);
+
+
+            return _grayscale_array;
+        }
+
+
+        public static Bitmap ByteToGrayBitmap(byte[] rawBytes, int width, int height) // метод для перевода байтового массива в серое изображение
+        {
+            Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, width, height),
+                                            ImageLockMode.WriteOnly, bitmap.PixelFormat);
+
+            Marshal.Copy(rawBytes, 0, bitmapData.Scan0, rawBytes.Length);
+            bitmap.UnlockBits(bitmapData);
+
+            var pal = bitmap.Palette;
+            for (int i = 0; i < 256; i++) pal.Entries[i] = Color.FromArgb(i, i, i);
+            bitmap.Palette = pal;
+
+
+            return bitmap;
+        }
+
+        public static byte Otsu(byte[] image)
+        {
+            int[] hist = new int[256];
+            for (int i = 0; i < image.Length; i++)
+                hist[image[i]]++;
+            int m = 0;
+            int n = 0;
+            for (int t = 0; t < 256; t++)
+            {
+                m += t * hist[t];
+                n += hist[t];
+            }
+
+            float maxSigma = -1;
+            int threshold = 0;
+            int alpha1 = 0;
+            int beta1 = 0;
+
+            for (int t = 0; t < 256; t++)
+            {
+                alpha1 += t * hist[t];
+                beta1 += hist[t];
+                float w1 = (float)beta1 / n;
+                float a = (float)alpha1 / beta1 - (float)(m - alpha1) / (n - beta1);
+                float sigma = w1 * (1 - w1) * a * a;
+                if (sigma > maxSigma)
+                {
+                    maxSigma = sigma;
+                    threshold = t;
+                }
+            }
+
+
+            return (byte)threshold;
+        }
+
+        public static byte[] Binarization(byte[] Img, byte Threshold)
+        {
+            for (int i = 0; i < Img.Length; i++)
+                Img[i] = Img[i] > Threshold ? (byte)255 : (byte)0;
+            return Img;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.Filter = imagefilterof;
+            if (of.ShowDialog() != DialogResult.Cancel)
+            {
+                DateTime start = DateTime.Now;
+                DefaultImg = new Bitmap(of.FileName);
+                pictureBox1.Image = DefaultImg;
+
+
+                var _grayscale_array = BitmapToGrayScaleByte(DefaultImg);
+
+
+                byte[] result = new byte[_grayscale_array.Length];
+                Buffer.BlockCopy(_grayscale_array, 0, result, 0, _grayscale_array.Length);
+
+                DateTime t1 = DateTime.Now;
+                GrayScaleImg = ByteToGrayBitmap(result, DefaultImg.Width, DefaultImg.Height);
+                pictureBox2.Image = GrayScaleImg;
+                DateTime t2 = DateTime.Now;
+
+                BinThreshold = Otsu(result);
+                result = Binarization(result, BinThreshold);
+                BinImg = ByteToGrayBitmap(result, DefaultImg.Width, DefaultImg.Height);
+                pictureBox3.Image = BinImg;
+
+                TimeSpan worktime = DateTime.Now - start - (t2 - t1);
+
+                toolStripStatusLabel1.Text = "Binarization threshold = " + BinThreshold + ". Done for " + worktime.ToString();
+
+
+                saveToolStripMenuItem.Enabled = true;
+                grayscaleToolStripMenuItem.Enabled = true;
+                binaryToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void grayscaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = imagefiltersf;
+            if (sf.ShowDialog() != DialogResult.Cancel)
+                pictureBox2.Image.Save(sf.FileName);
+        }
+
+        private void binaryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = imagefiltersf;
+            if (sf.ShowDialog() != DialogResult.Cancel)
+                pictureBox3.Image.Save(sf.FileName);
+        }
+    }
+}
